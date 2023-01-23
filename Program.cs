2eaa9b824bdf14bbb4e2cccaba906e2d949b1a01
Marcus Westin <marcus@sekurbit.se>
2023-01-23 09:04:31 +0100

    issue code

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// This is the main entry point of the application.
+
+using System;
+using AppKit;
+using ScreenCaptureKit;
+
+NSApplication.Init();
+
+Console.WriteLine("Nu kör vi!");
+
+var test = await SCShareableContent.GetShareableContentAsync(false, true);
+foreach (var w in test.Windows)
+    Console.WriteLine("Window: " + w.Title);
